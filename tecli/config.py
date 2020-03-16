edb0b17e84272ffb38457a2d1fa54a7e28c2fa72
Alex Zvoleff <azvoleff@gmail.com>
2020-03-16 16:08:51 -0400

    Fix settings variable access,

--- tecli/config.py
+++ tecli/config.py
@@ -11,42 +11,46 @@ import logging
 config_path = os.path.expanduser('~') + '/.tecli.yml'
 
 # Default values that can be altered in local .tecli.yml file
-config = {
+settings = {
     'url_api': 'https://api.trends.earth'
 }
 
 def set(var_name, value):
+    global settings
     with open(config_path, 'r+') as infile:
-        config.update(yaml.load(infile, Loader=yaml.FullLoader))
+        settings.update(yaml.load(infile, Loader=yaml.FullLoader))
         with open(config_path, 'w+') as outfile:
-            if config is None:
-                config = {}
-            config[var_name] = value
-            yaml.dump(config, outfile, default_flow_style=False)
+            if settings is None:
+                settings = {}
+            settings[var_name] = value
+            yaml.dump(settings, outfile, default_flow_style=False)
     return True
 
 def show(var_name, value):
+    global settings
     with open(config_path, 'r+') as infile:
-        config.update(yaml.load(infile, Loader=yaml.FullLoader))
-        if config is not None:
-            print('Value: ' + str(config[var_name]))
+        settings.update(yaml.load(infile, Loader=yaml.FullLoader))
+        if settings is not None:
+            print('Value: ' + str(settings[var_name]))
     return True
 
 def get(var_name):
+    global settings
     with open(config_path, 'r+') as infile:
-        config.update(yaml.load(infile, Loader=yaml.FullLoader))
-        if config is not None and var_name in config:
-            return config[var_name]
+        settings.update(yaml.load(infile, Loader=yaml.FullLoader))
+        if settings is not None and var_name in settings:
+            return settings[var_name]
         return ''
     return True
 
 def unset(var_name, value):
+    global settings
     with open(config_path, 'r+') as infile:
-        config.update(yaml.load(infile, Loader=yaml.FullLoader))
+        settings.update(yaml.load(infile, Loader=yaml.FullLoader))
         with open(config_path, 'w+') as outfile:
-            if config is not None:
-                config.pop(var_name, None)
-                yaml.dump(config, outfile, default_flow_style=False)
+            if settings is not None:
+                settings.pop(var_name, None)
+                yaml.dump(settings, outfile, default_flow_style=False)
     return True
 
 ACTIONS = {
